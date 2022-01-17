0de439195024b4dcd83e3a8e3c4cf3ed735419c4
blur1379 <blor1379@gmail.com>
2022-01-17 18:53:57 +0330

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Blur on 10/27/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
